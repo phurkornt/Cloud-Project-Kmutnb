a8c026b19f640539d1fdfbf2ebedf263db228a00
phurikornt <mysteriousphu@gmail.com>
2023-04-07 22:57:14 +0700

    fetch -> insert lottery

--- src/controllers/admin/admin_lottery.rs
+++ src/controllers/admin/admin_lottery.rs
@@ -1,16 +1,51 @@
 
 use actix_web::{web, get ,post ,delete , Responder, HttpResponse, http::StatusCode};
 
+use log::debug;
+
+
+use crate::models::admin_lotterry_model::*;
+use rand::seq::SliceRandom;
+
 
 #[get("/admin/lottery")]
-async fn get_admin_lottery() -> impl Responder {
-   
-    
-    return HttpResponse::Ok().json("OK get admin");
+async fn get_admin_lottery(admin: web::Json<AdminID>) -> impl Responder {
+
+    if admin.admin_id == 1{
+        let all_date = get_date_unique();
+        let mut lottery_detail:Vec<LotteryDateCount> =  Default::default();
+        for i in all_date{
+            lottery_detail.push(get_lottery(i.date));
+        }
+        // debug!("Test Get {:?}",lottery_detail);
+        return HttpResponse::Ok().json(lottery_detail);
+
+    }else{
+
+        return HttpResponse::Unauthorized().json("Error");
+    }
+
+
 }
+
+
+
 #[post("/admin/lottery")]
-async fn post_admin_lottery() -> impl Responder {
-   
+async fn post_admin_lottery(admin: web::Json<AdminIDCount>) -> impl Responder {
+    let admin = admin.into_inner();
+    if admin.admin_id == 1{
+        let number = random_numbers(admin.lottery_count as usize);
+        for i in 0..admin.lottery_count{
+            debug!("Test Get {:?}","1");
+            insert_lottery(number[i as usize].to_string());
+        }
+
+        return HttpResponse::Ok().json("lottery_detail");
+
+    }else{
+
+        return HttpResponse::Unauthorized().json("Error");
+    }
     
     return HttpResponse::Ok().json("OK post admin");
 }
@@ -19,4 +54,29 @@ async fn delete_admin_lottery() -> impl Responder {
    
     
     return HttpResponse::Ok().json("OK delete admin");
+}
+
+
+
+
+
+
+
+/* ---------------------------- Function ---------------------------- */
+
+fn random_numbers(n: usize) -> Vec<String> {
+    let mut numbers: Vec<String> = Vec::new();
+    let digits: Vec<u8> = (0..=9).collect();
+    let mut rng = rand::thread_rng();
+    
+    while numbers.len() < n {
+        let mut nums: Vec<u8> = digits.clone();
+        nums.shuffle(&mut rng);
+        let num_str = nums[..6].iter().map(|d| d.to_string()).collect::<String>();
+        if !numbers.contains(&num_str) {
+            numbers.push(num_str);
+        }
+    }
+    
+    numbers
 }
--- src/models/admin_lotterry_model.rs
+++ src/models/admin_lotterry_model.rs
@@ -2,10 +2,133 @@ use serde::{Serialize, Deserialize};
 use mysql::*;
 use mysql::prelude::*;
 use crate::config::db::conDB;
+use chrono::{Local, DateTime};
 
+use rand::seq::SliceRandom;
+
+
+//  ---------------------------------- Data Input ----------------------------------
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AdminID {
+    pub admin_id: u32,
+}
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AdminIDCount {
+    pub admin_id: u32,
+    pub lottery_count:u32
+}
+
+
+
+
+
+//  ---------------------------------- Data Output ----------------------------------
 
 #[derive(Debug,Serialize, Deserialize)]
-pub struct Lottery {
-    pub lottery_id: u32,
-    pub lottery_number: String,
+pub struct LotteryDateCount {
+    pub date: String,
+    pub lottery_count: u32
+}
+
+#[derive(Debug,Serialize, Deserialize)]
+pub struct Date {
+    pub date: String,
+}
+
+
+
+
+// ####################### get_reward(ดูรางวัลทั้งหมด) reward_number, DATE(Datetime) #######################
+pub fn get_lottery(date:String) -> LotteryDateCount{
+
+    let db = conDB()
+    .map(|mut conn| {
+        conn.query_map(
+            // เพิม่ Date ด้วย
+            "SELECT Date(Datetime) as date , COUNT(lottery_number) as count FROM lottery WHERE Date(Datetime) = '".to_owned() + date.as_str()+" ' ",
+            |(date , lottery_count)| {
+                LotteryDateCount 
+                {   
+                    date,
+                    lottery_count
+                }
+            },
+        )
+    })
+    .unwrap_or_else(|_| {
+        Ok(Vec::new())
+    });
+   
+    let mut data:LotteryDateCount = LotteryDateCount { date: String::new(), lottery_count: 0 };
+    match db {
+        Ok(lotteries) => {
+            for i in lotteries{
+                data =  i;
+                break;
+            }
+        }
+        Err(e) => println!("Error: {}", e)
+    }
+
+    return data;
+    
+}
+
+
+
+
+// ####################### get_date_unique (วันที่เพื่อนำไปประกอบข้อมูล) ->["11/11/111" , "11/11/111"] #######################
+pub fn get_date_unique() -> Vec<Date>{
+    let db = conDB()
+    .map(|mut conn| {
+        conn.query_map(
+            // เพิม่ Date ด้วย
+            "SELECT DISTINCT Date(Datetime) FROM lottery;",
+            |date| {
+                Date 
+                {   
+                    date
+                }
+            },
+        )
+    })
+    .unwrap_or_else(|_| {
+        Ok(Vec::new())
+    });
+   
+    let mut data:Vec<Date> =  Default::default();
+    match db {
+        Ok(lotteries) => {
+            for i in lotteries{
+                data.push(i);
+            }
+        }
+        Err(e) => println!("Error: {}", e)
+    }
+
+    return data;
+    
+}
+
+
+
+// ####################### get_date_unique (วันที่เพื่อนำไปประกอบข้อมูล) ->["11/11/111" , "11/11/111"] #######################
+pub fn insert_lottery(lottery_number:String){
+    let time_now: DateTime<Local> = Local::now();
+    let formatted = time_now.format("%Y-%m-%d %H:%M:%S").to_string();
+
+    let _ = match conDB() {
+        Ok(mut conn) => {
+            conn.exec_drop(
+            "INSERT INTO lottery(lottery_number, lottery_status, Datetime) VALUES (:lottery_number,'available',:time_now)",
+            params! {
+                "lottery_number" => lottery_number,
+                "time_now" => formatted
+            },
+        )},
+        Err(e) => {
+            println!("Failed to get DB connection: {}", e);
+            return;
+        }
+    };
 }
